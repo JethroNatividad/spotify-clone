9345fa94f1e32c34783553b0ea95f1eefc6ac8b8
JethroSama <kel73896@gmail.com>
2022-03-17 16:29:26 +0800

    add sidebar component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
     <div >
-      <p className='text-green-600'>Works</p>
+      <Head>
+        <title>Spotify clone</title>
+      </Head>
+      <main>
+        {/* sidebar */ }
+        <Sidebar />
+        {/* songs */ }
+      </main>
+
+      <div>
+        {/* bottom play */ }
+      </div>
     </div>
   )
 }
