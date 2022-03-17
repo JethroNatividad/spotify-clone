f427a69227192087cc8911dec373752a69ed571b
JethroSama <kel73896@gmail.com>
2022-03-17 16:47:49 +0800

    add more sidebar items

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { HomeIcon, LibraryIcon, SearchIcon } from '@heroicons/react/outline'
+import { HeartIcon, HomeIcon, LibraryIcon, PlusCircleIcon, RssIcon, SearchIcon } from '@heroicons/react/outline'
 import SidebarItem from './SidebarItem'
 const Sidebar = () => {
     return (
-        <div>
-            <div>
+        <div className='text-gray-600'>
+            <div className='p-5 space-y-2'>
                 <SidebarItem Icon={ HomeIcon } label="Home" />
                 <SidebarItem Icon={ SearchIcon } label="Search" />
                 <SidebarItem Icon={ LibraryIcon } label="Your library" />
+                <hr className='border-t-1 border-gray-900' />
+                <SidebarItem Icon={ PlusCircleIcon } label="Create Playlist" />
+                <SidebarItem Icon={ HeartIcon } label="Liked Songs" />
+                <SidebarItem Icon={ RssIcon } label="Your Episodes" />
+                <hr className='border-t-1 border-gray-900' />
             </div>
         </div>
     )
--- components/SidebarItem.js
+++ components/SidebarItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SidebarItem = ({ label, Icon, ...props }) => {
     return (
-        <button { ...props } className='flex'>
+        <button { ...props } className='flex items-center space-x-2 text-sm md:text-lg hover:text-white'>
             <Icon className='w-5 h-5' />
             <p>{ label }</p>
         </button>
