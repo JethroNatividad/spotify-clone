0e8b19b139cd7560fd55f67f9d5dac447fdce7e8
JethroSama <kel73896@gmail.com>
2022-03-21 13:39:36 +0800

    Implement Next auth spotify

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,2 +1,17 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from 'next-auth/providers/spotify'
+
+export default NextAuth({
+    providers: [
+        SpotifyProvider({
+            clientId: process.env.SPOTIFY_CLIENT_ID,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+            authorization: 'https://accounts.spotify.com/authorize?scope=user-read-email'
+        })
+    ],
+    secret: process.env.JWT_SECRET,
+    pages: {
+        signIn: '/login'
+    },
+
+})
---
+++ pages/login.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { getProviders, signIn, signOut, useSession } from "next-auth/react"
+
+export async function getServerSideProps(ctx) {
+    const providers = await getProviders()
+    return {
+        props: { providers }
+    }
+}
+
+const login = ({ providers }) => {
+    const { spotify } = providers
+    const { data: session } = useSession()
+    console.log(providers)
+    console.log("SESSION FRONT >", session)
+    if (session) {
+        return (
+            <>
+                Signed in as { session.user.name } <br />
+                <button onClick={ () => signOut() }>Sign out</button>
+            </>
+        )
+    }
+    return (
+        <>
+            Not signed in <br />
+            <button onClick={ () => signIn(spotify.id) }>Sign in with spotify</button>
+        </>
+    )
+}
+
+export default login
