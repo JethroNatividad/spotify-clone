89936ede7d74f31e2f17be2daab8c661d5d3af0d
JethroSama <kel73896@gmail.com>
2022-03-24 18:30:27 +0800

    add middleware to check auth

---
+++ pages/_middleware.js
@@ -0,0 +1,21 @@
+import { getToken } from "next-auth/jwt"
+import { NextResponse } from "next/server"
+
+
+export default async function(req) {
+
+    const jwt = await getToken({ req, secret: process.env.JWT_SECRET })
+    console.log("JWT MIDDLEWARE >", jwt)
+    console.log(req.nextUrl)
+    const { pathname, origin } = req.nextUrl
+
+    if (pathname.includes("api/auth/") || jwt) {
+        console.log("continue")
+        return NextResponse.next()
+    }
+
+    if (!jwt && pathname !== "/login") {
+        console.log("NO JWT")
+        return NextResponse.redirect(origin + "/login")
+    }
+}
