982b8f89e0e719c77e409c7bae53c42fe3edfd99
JethroSama <kel73896@gmail.com>
2022-03-24 18:41:53 +0800

    add sign out button

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { HeartIcon, HomeIcon, LibraryIcon, PlusCircleIcon, RssIcon, SearchIcon } from '@heroicons/react/outline'
 import SidebarItem from './SidebarItem'
+import { useSession } from 'next-auth/react'
 const Sidebar = () => {
+    const { data: session } = useSession()
+    console.log(session)
     return (
         <div className='text-gray-600'>
             <div className='p-5 space-y-2'>
+                { session && (
+                    <button className='flex items-center space-x-2 text-sm md:text-lg hover:text-white'>
+                        <p>Sign out</p>
+                    </button>
+                ) }
                 <SidebarItem Icon={ HomeIcon } label="Home" />
                 <SidebarItem Icon={ SearchIcon } label="Search" />
                 <SidebarItem Icon={ LibraryIcon } label="Your library" />
